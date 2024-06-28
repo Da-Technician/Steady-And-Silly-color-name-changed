92258344e2d5d72aad2e853a9e42295fe7be3476
vr-thecnician <165536017+vr-thecnician@users.noreply.github.com>
2024-06-27 23:29:50 -0400

    Add files via upload

---
+++ StandRig.cs
@@ -0,0 +1,28 @@
+﻿using GorillaShirts.Tools;
+using System;
+using UnityEngine;
+
+namespace GorillaShirts.Models
+{
+    public class StandRig : Rig
+    {
+        public event Action<Configuration.PreviewTypes> OnAppearanceChange;
+
+        public Color
+            SteadyColour = new(0.9f, 0.9f, 0.9f),
+            SillyColour = new(0.9f, 0.9f, 0.0f);
+
+        public MeshRenderer
+            SteadyHat, SillyHat;
+
+        public void SetAppearance(bool isSilly)
+        {
+            SteadyHat.enabled = !isSilly;
+            SillyHat.enabled = isSilly;
+            RigSkin.material.color = isSilly ? SillyColour : SteadyColour;
+            Nametag.text = isSilly ? "YELLOW" : "WHITE";
+
+            OnAppearanceChange?.Invoke(isSilly ? Configuration.PreviewTypes.Silly : Configuration.PreviewTypes.Steady);
+        }
+    }
+}
